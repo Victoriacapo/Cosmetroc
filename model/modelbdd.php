d7a61857d2e0b73edbd747d0472be91943f55753
Victoriacapo <43335853+Victoriacapo@users.noreply.github.com>
2019-02-15 14:44:12 +0100

    validé

--- model/modelbdd.php
+++ model/modelbdd.php
@@ -6,7 +6,7 @@ class database { //declaration class database
 
     public function __construct() { //methode magique construct permet d'executer ce qu'il y a ds le construct et permet d'executer le destruct a la fin a la fin de l'instanciation
         try {
-            $this->database = new PDO('mysql:host=localhost;dbname=cosmetroc;charset=utf8', 'victoriac', '1Travail'); //instancie un nouvel objet; permet la connexion a ma base de donnée.
+            $this->database = new PDO('mysql:host=localhost;dbname=cosmetroc;charset=utf8', 'victoriac', 'victoriac'); //instancie un nouvel objet; permet la connexion a ma base de donnée.
         } catch (Exception $error) {
             die('Erreur : ' . $error->getMessage()); //affiche les msg d'erreur de connexion a la base de donnee
         }
